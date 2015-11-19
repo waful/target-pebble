f923e6a476ed9872ae06d588dd1414e66c532dd4
Wanfung Lee <cloudwindhk@gmail.com>
2015-11-18 17:11:53 -0800

    no need seconds

--- src/main.c
+++ src/main.c
@@ -9,14 +9,12 @@ static bool BT_VIBE;
 static void tick_handler(struct tm *time_now, TimeUnits changed) {
   static bool first_run = true;
 
-  if(first_run || (changed & MINUTE_UNIT)){
-    // Convert to 24h
-    char the_24h_buffer[] = "00";
-    strftime(the_24h_buffer, sizeof(the_24h_buffer), "%H", time_now);
-    uint8_t hour_buffer = (the_24h_buffer[0] - '0') * 10 + (the_24h_buffer[1] - '0');
+  // Convert to 24h
+  char the_24h_buffer[] = "00";
+  strftime(the_24h_buffer, sizeof(the_24h_buffer), "%H", time_now);
+  uint8_t hour_buffer = (the_24h_buffer[0] - '0') * 10 + (the_24h_buffer[1] - '0');
 
-    main_window_minute_update(hour_buffer, time_now->tm_min);
-  }
+  main_window_minute_update(hour_buffer, time_now->tm_min);
 
   if(first_run || (changed & DAY_UNIT)){
     main_window_date_update(time_now->tm_mday);
@@ -63,7 +61,7 @@ static void init() {
   main_window_set_config(getMinutes_color(), getMinutes_no_bt_color(), getHours_color(), getHours_no_bt_color(), getText_color(), getText_low_battery_color(), getBg_color(), getBar_radius(), getBar_offset(), getRing_markings());
   BT_VIBE = getBt_vibe();
   main_window_init();
-  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   battery_state_service_subscribe(battery_callback);
   battery_callback(battery_state_service_peek());
   bluetooth_connection_service_subscribe(bluetooth_callback);
